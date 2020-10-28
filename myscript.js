dce97f04a7837b38964273137725b61b350d4c0a
Rusakova <60939575+rusakovalarysa@users.noreply.github.com>
2020-10-28 10:22:39 +0200

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,13 @@
+﻿function openTab(evt, TabName) {
+  var i, tabcontent, navlinks;
+  tabcontent = document.getElementsByClassName("rowsect");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  navlinks = document.getElementsByClassName("nav-link");
+  for (i = 0; i < navlinks.length; i++) {
+    navlinks[i].className = navlinks[i].className.replace(" active", "");
+  }
+  document.getElementById(TabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
