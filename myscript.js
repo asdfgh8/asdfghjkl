21a1fe8bedd1026edebd471a6fc5bfa9c4cac28c
Rusakova <60939575+rusakovalarysa@users.noreply.github.com>
2020-11-05 10:34:05 +0200

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -24,27 +24,43 @@ basket.add = function (name, priceforone) {
 }
 
 // Необхідні змінні
-var sum=0, count=0, zagal=0;
-//Функція для підрахунку загальної суми покупки
+var sum, count=[], zagal=0, countofprod=[], tempor=[];
+//Функція, що розраховує загальну суму покупки
 function modifySum (inp) {
-count = Number(inp);
-sum=count * c;
-document.getElementById("zagsum").innerHTML = zagal;
+count = document.getElementsByClassName("totcount");
+for(var i = 0; i < count.length; i++){
+    countofprod[i] = count[i].value;
+    }
+countofprod.reverse();
+//Підрахунок та виведення суми за певний товар
+sum = document.getElementsByClassName("zag");
+for(var i = 0; i < countofprod.length; i++){
+    tempor[i] = Number(countofprod[i]) * Number(basket[i].priceforone);
+    }
+tempor.reverse();
+for(var i = 0; i < countofprod.length; i++){
+    sum[i].innerHTML = tempor[i];
+    }
+//Підрахунок та виведення загальної суми покупки
+for(var i = 0; i < tempor.length; i++){
+    zagal += Number(tempor[i]);
+    }
+document.getElementById("zagsum").innerHTML = `${zagal} грн.`;
+document.getElementById("totals").innerHTML = `Загальна сума покупки: ${zagal} грн.`;
+zagal = 0;
 }
 
 // Функція, що додає товари в кошик
 function addToBasket (evt, TabName, a, b) {
-  var i, d=0, tabcontent, navlinks;
+  var d=0, tabcontent, navlinks;
 c = Number(b);
-
   tabcontent = document.getElementsByClassName("rowsect");
-  for (i = 0; i < tabcontent.length; i++) {
+  for ( var i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
   document.getElementById(TabName).style.display = "block";
 basket.add(a, b);
 var tabl=document.getElementById("purchase");
 var newrow = tabl.insertRow(0); // Додавання нового рядка в таблицю
-newrow.innerHTML = `<td>${a}</td><td>${b}</td><td><input class="form-control" type="number" placeholder="1" min="1" max="10" onchange="modifySum(value)"></td>`;
-zagal +=sum;
+newrow.innerHTML = `<td>${a}</td><td>${b}</td><td class="zag"></td><td><input class="form-control totcount" type="number" placeholder="0" min="1" max="10" onchange="modifySum(value)"></td>`;
 }
